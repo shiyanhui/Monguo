35faf11a108a2e3e38773f7a2835dfe92f141a9a
shiyanhui <shiyanhui66@gmail.com>
2013-10-05 16:42:05 +0800

    buid

---
+++ doc/conf.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+html_theme = 'sphinxdoc'
---
+++ monguo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+
+def get_version():
+	if isinstance(VERSION[-1], basestring):
+		return '.'.join(map(str, VERSION[:-1])) + VERSION[-1]
+	return '.'.join(map(str, VERSION))
+
+__version__ = get_version()
+
