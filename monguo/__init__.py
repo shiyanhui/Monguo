e7516599b46677e2176bdad74b3bb527d10c396b
shiyanhui <shiyanhui66@gmail.com>
2013-10-09 15:13:33 +0800

    tab -> spaces

--- monguo/__init__.py
+++ monguo/__init__.py
@@ -3,8 +3,8 @@
 VERSION = (0, 1)
 
 def get_version():
-	if isinstance(VERSION[-1], basestring):
-		return '.'.join(map(str, VERSION[:-1])) + VERSION[-1]
-	return '.'.join(map(str, VERSION))
+    if isinstance(VERSION[-1], basestring):
+        return '.'.join(map(str, VERSION[:-1])) + VERSION[-1]
+    return '.'.join(map(str, VERSION))
 
 __version__ = get_version()
