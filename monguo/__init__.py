c4d46ad21d919a12a3b8271502da85347fd1b5df
lime <shiyanhui66@gmail.com>
2013-11-25 11:03:09 +0800

    fix

--- monguo/__init__.py
+++ monguo/__init__.py
@@ -3,11 +3,11 @@
 # @Author: lime
 # @Date:   2013-10-25 19:45:09
 # @Last Modified by:   lime
-# @Last Modified time: 2013-11-11 20:21:16
+# @Last Modified time: 2013-11-25 11:02:40
 
 '''Monguo, an asynchronous MongoDB ORM for Tornado'''
 
-VERSION = (0, 1, 1)
+VERSION = (0, 1, 2)
 
 def get_version():
     if isinstance(VERSION[-1], basestring):
